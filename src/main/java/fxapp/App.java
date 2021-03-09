f7ef72d76c8f1765c693af9e71b78ce9aa634a07
IgnatiusGL <2370291851@qq.com>
2021-03-09 14:14:00 +0800

    init project

---
+++ src/main/java/fxapp/App.java
@@ -0,0 +1,20 @@
+package fxapp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        var label = new Label("Hello, JavaFX");
+        var scene = new Scene(label,600,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module fxmodule {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+
+    exports fxapp;
+}
